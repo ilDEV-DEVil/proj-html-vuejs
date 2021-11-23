29fd52ea07f190aac9c57001529b67941dc5ec14
flashbLack98 <npalakuttige98@icloud.com>
2021-11-23 01:00:05 +0100

    add textimg component

---
+++ src/components/TextImg.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="d-flex my_container">
+    
+    <CardText></CardText>
+    <div class="containerImg">    
+        <img src="@/assets/img/slider52x.jpg"  alt="">
+    </div>   
+
+  </div>
+</template>
+
+<script>
+
+import CardText from "./CardText.vue"
+
+export default {
+name: "TextImg",
+components: { CardText, },
+/* props: {
+    imgSrc : String,
+    }
+ */}
+</script>
+
+<style lang="scss" >
+.my_container{
+    width: 100%;
+    height: 510px;
+    .containerImg{
+        width: 55%;
+        height: 100%;
+        img{
+            height: 100%;
+            width:100%
+        }
+    }
+}
+
+</style>
